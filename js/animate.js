9b36e2edbe7b9cc6e24f91a5527f6f1e2efb04a6
ChuotNhat <tranhuong13520340@gmail.com>
2017-07-17 23:32:43 +0700

    ChuotNhat
    ChuotNhat

---
+++ js/animate.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	wow = new WOW(
+		{
+			boxClass:     'wow',      // default
+			animateClass: 'animated', // default
+			offset:       0,          // default
+			mobile:       true,       // default
+			live:         true        // default
+
+		}
+	)
+	wow.init();
+	$(window).bind('scroll', function(){
+		var x = $(window).scrollTop();
+		$("#bind").text(x);
+		if(x>=100) {
+			$("#intro-wow").css("display","block");
+			$("#intro-small").css("display","block");
+		}
+		if(x>=1365) {
+			$(".content-work img").attr("src", "img/work2.png");
+		}
+		else {
+			$(".content-work img").attr("src", "img/work1.png");
+		}
+		if(x>1600) {
+			$(".content-work img").attr("src", "img/work3.png");
+		}
+		if(x>=2301) {
+			$(".umberla").css("position", "fixed");
+		}
+		else {
+			$(".umberla").css("position", "absolute");
+		}
+		if(x>=3513) {
+			$(".umberla").css("display", "none");
+		}
+		else {
+			$(".umberla").css("display", "block");
+		}
+		if(x>3800) {
+			$(".full-song").css("background-position", (x-3948)/4+"px 0px"  );
+			$(".full-song1").css("background-position", (3948-x)/4+"px 0px"  );
+		}
+		if(x> 4220) {
+			$(".ong-hut").css("top", -300 + 1.06*x-4220 + "px");
+		}
+	});
+
+});
